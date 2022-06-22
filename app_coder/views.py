82c9a2dee8375edcb765c7cba79ce92fb4f26d79
FrancisEspo <esposito.francisco9@gmail.com>
2022-06-22 20:55:37 -0300

    segundo commit

--- app_coder/views.py
+++ app_coder/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from app_coder.models import Curso
 
 def curso(self):
-    curso = Curso(nombre = "Python", camada = "1345")
+    curso = Curso(nombre = "django", camada = "1345")
     curso.save()
 
     documento = f"El curso es: {curso.nombre}, de la camada: {curso.camada}"
