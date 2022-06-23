ee0094d7a34d4878c964ae0478d87c45242afbcd
FrancisEspo <esposito.francisco9@gmail.com>
2022-06-22 21:07:06 -0300

    agregando cargadores de templates

--- app_coder/views.py
+++ app_coder/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from app_coder.models import Curso
+from django.template import loader
 
+# Create your views here.
 def curso(self):
     curso = Curso(nombre = "django", camada = "1345")
     curso.save()
@@ -9,5 +11,3 @@ def curso(self):
     documento = f"El curso es: {curso.nombre}, de la camada: {curso.camada}"
 
     return HttpResponse(documento)
-
-# Create your views here.
